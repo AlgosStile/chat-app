44882f058aaa31adbcea94843f9f8665447b065d
AlgosStile <oleg.todor@mail.ru>
2025-04-03 19:43:52 +0300

    push app

---
+++ src/main/java/server/WebSocketServer.java
@@ -0,0 +1,59 @@
+package server;
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+
+import java.net.URISyntaxException;
+
+/**
+ * Класс WebSocketServer отвечает за создание и запуск WebSocket-сервера.
+ * Сервер принимает подключения клиентов и обрабатывает WebSocket-соединения.
+ */
+public class WebSocketServer {
+    private final Server server;
+
+    public WebSocketServer(int port) throws URISyntaxException {
+        this.server = new Server(port);
+
+        // Конфигурация статических файлов
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setDirectoriesListed(false);
+        resourceHandler.setResourceBase(
+                getClass().getClassLoader().getResource("static").toURI().toString()
+        );
+
+        /**
+         * Конфигурация WebSocket
+         */
+        ServletContextHandler wsHandler = new ServletContextHandler();
+        wsHandler.setContextPath("/");
+        wsHandler.addServlet(new ServletHolder(new WebSocketServlet() {
+            @Override
+            public void configure(WebSocketServletFactory factory) {
+                factory.register(WebSocketHandler.class);
+            }
+        }), "/chat");
+
+        /**
+         * Объединение обработчиков
+         */
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resourceHandler, wsHandler});
+        server.setHandler(handlers);
+    }
+
+    public void start() throws Exception {
+        server.start();
+        server.join();
+    }
+
+    public void stop() throws Exception {
+        server.stop();
+    }
+}
